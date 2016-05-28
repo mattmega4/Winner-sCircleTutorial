3a82c5139b7c92c0afe84d8eecf13672ae80c933
Matthew Howes Singleton <Singleton@matthews-macbook-pro.local>
2016-05-27 17:51:32 -0700

    Initial Commit

---
+++ Winner'sCircleTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Winner'sCircleTutorial
+//
+//  Created by Matthew Howes Singleton on 5/27/16.
+//  Copyright © 2016 Matthew Howes Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
