f499c5cb8b2bf7f52b5a26e83718f64fec701a74
Matthew Howes Singleton <Singleton@Matthews-MacBook-Pro.local>
2016-05-28 16:38:02 -0700

    first commit

--- Winner'sCircleTutorial/LandingPageViewController.swift
+++ Winner'sCircleTutorial/LandingPageViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LandingPageViewController.swift
 //  Winner'sCircleTutorial
 //
 //  Created by Matthew Howes Singleton on 5/27/16.
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LandingPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
 
 
 }
-
